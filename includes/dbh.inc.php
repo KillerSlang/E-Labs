a052565791367618818003cbaf757893ff67e8db
EJZandberg <73105419+EJZandberg@users.noreply.github.com>
2020-12-08 18:32:30 +0100

    update database handler

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -20,9 +20,24 @@
         mysqli_close($GLOBALS['conn']);
     }
   }
-// bind result kun je met $GLOBALS['stmt'] aanroepen zien hieronder voor voorbeeld.
-
-/* 
+// bind result kun je gewoon met $stmt aanroepen zie hieronder voor voorbeelden.
+/* titel van labjournalen en loboeken van studentID 1 ophalen.
+    queryAanmaken('SELECT `labjournaalTitel`,`logboek` 
+                   FROM `labjournaal` 
+                   WHERE `studentID` = 1;');
+    mysqli_stmt_bind_result($stmt, $titelLabjournaal, $logboek);
+    mysqli_stmt_store_result($stmt);
+    if(mysqli_stmt_num_rows($stmt) != 0)
+    {
+        echo "Labjournalen en logboeken van studentID 1 <hr />";
+        while(mysqli_stmt_fetch($stmt))
+        {
+            echo $titelLabjournaal."&nbsp; &nbsp;". $logboek."<br />";
+        }
+    }
+    querySluiten();
+*/
+/* titellabjournal van studentID 1 en docentID 1 toevoegen.
     $titelLabjournaal =  filter_input(INPUT_POST,'titelLabjournaal', FILTER_SANITIZE_SPECIAL_CHARS); 
     queryAanmaken('INSERT INTO labjournaal(studentID,docentID,labjournaalTitel)
     VALUES ("1","1","'.$titelLabjournaal.'");');
