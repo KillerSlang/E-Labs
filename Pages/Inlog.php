b5d15afcbfa36eb4513285ea6f120da0c5f9b4bf
peter <72741411+PeterB2003@users.noreply.github.com>
2020-12-10 14:51:33 +0100

    inlog pagina begin
    inlog pagina begin

---
+++ Pages/Inlog.php
@@ -0,0 +1,27 @@
+
+<h1> Welkom! </h1>
+
+<form action = "" method = "POST">
+
+<p>email: <input type = "email" name = "Email"></p>
+<p>wachtwoord: <input type = "password" name = "Password"></p>
+
+<p><a href = "" >Wachtwoord vergeten?</a></p>
+
+<p><input type = "radio" name = "SorD" value = "Student">Student
+<input type = "radio" name = "SorD" value = "Docent">Docent</p>
+
+
+<p>
+    <button formaction = "">Registreren</button>
+    <input type = "submit" name = "Submit" value = "Submit">
+</p>
+
+</form>
+
+
+<?php
+
+    $_POST
+
+?>
