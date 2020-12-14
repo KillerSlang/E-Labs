845d9a718dbc946144df8a2845e8b6bffb726e12
Julian Jager <julianjager2011@gmail.com>
2020-12-14 15:40:20 +0100

    Labjournaal Formulier updates
    samen met erik jan
    
    Co-Authored-By: EJZandberg <73105419+EJZandberg@users.noreply.github.com>

---
+++ Pages/NewProtocol.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML>
+<head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="../Css/Main.css">
+    <link rel="stylesheet" href="../Css/Responsive.css">
+
+</head>
+<body>
+    <?php 
+    /* Header */
+    include_once '../Include/Header.php';
+    include_once '../Include/Dbh.inc.php';
+    ?>
+    <main id="Protocol">
+        <div class="PageTitle">
+            <h1>Nieuw Protocol</h1>
+            <hr>
+        </div>
+        <div class="whitebg">
+            <div class="content">
+                <form class="Pform" method="POST" action="NewProtocol.php" enctype="multipart/form-data">
+                    
+                    <label for="PTitle">Protocol Titel: *</label>
+                    <input type="text" name="PTitle" placeholder="Titel" required></input><br>
+                    
+                    <label for="Vakken">Vak: *</label>
+                    <div name="Vakken">
+                        <input type="radio" id="BML" name="PVak" value="BML" checked>
+                        <label for="BML">BML</label><br>
+                        <input type="radio" id="Chemie" name="PVak" value="Chemie">
+                        <label for="Chemie">Chemie</label>
+                    </div>
+
+                    <label for="Jaren">Jaar: *</label>
+                    <div name="Jaren">
+                        <input type="radio" id="Jaar 1" name="PJaar" value="1" checked>
+                        <label for="BML">Jaar 1</label><br>
+                        <input type="radio" id="Jaar 2" name="PJaar" value="2">
+                        <label for="Chemie">Jaar 2</label><br>
+                        <input type="radio" id="Jaar 3" name="PJaar" value="3">
+                        <label for="Chemie">Jaar 3</label>
+                    </div>
+
+                    <label for="PUpload">Protocol: *</label>
+                    <input type="file" name="PUpload" id="PUpload"><br>
+                    
+
+                    <input class="bluebtn" id="PSubmit" type="submit" value="Upload protocol" name="PSubmit">
+                </form>
+                <?php
+                
+                if(isset($_POST["PSubmit"])){
+                    if(!empty($_POST["PTitle"])){
+                        if(!empty($_POST["PVak"])){
+                            if(!empty($_POST["PJaar"])){
+                                if(!empty($_FILES)){
+                                
+                                    $studentID =  filter_var($_SESSION["studentID"], FILTER_SANITIZE_SPECIAL_CHARS);
+                                    $uploadDatum =  date("Y-m-d");
+                                    $titel =  filter_input(INPUT_POST,'PTitle', FILTER_SANITIZE_SPECIAL_CHARS);
+
+                                    //Protocol
+                                    $fileName = basename($_FILES["PUpload"]["name"]); 
+                                    $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+                                    
+                                    if($fileType == 'pdf'){ 
+                                        $pdf = $_FILES['PUpload']['tmp_name']; 
+                                        $pdfContent = addslashes(file_get_contents($pdf)); 
+
+                                        $vakken =  filter_input(INPUT_POST,'PVak', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $jaar =  filter_input(INPUT_POST,'PJaar', FILTER_SANITIZE_SPECIAL_CHARS); 
+                                        
+                                        queryAanmaken('INSERT INTO protocol(studentID,uploadDatum,titel,protocol,vakken,jaar)
+                                        VALUES ("'.$studentID.'","'.$uploadDatum.'","'.$titel.'","'.$pdfContent.'","'.$vakken.'","'.$jaar.'")');
+                                        querySluiten();
+                                        
+                                    }
+                                    else {
+                                        echo "Je mag alleen een .pdf bestand uploaden.";
+                                    }
+
+
+                                    
+                                }else{
+                                    echo "Geen Bestand geselecteerd";
+                                }
+                            }else{
+                                echo "Geen Jaar geselecteerd";
+                            }
+                        }else{
+                            echo "Geen Vak geselecteerd";
+                        }
+                    }else{
+                        echo "Geen Titel ingevoerd";
+                    }
+                }
+                ?>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+    </main>
+    <?php 
+    /* Footer */
+    include_once '../Include/Footer.php';
+    ?>    
+</body>
--- includes/addlabjournaal.inc.php
+++ includes/addlabjournaal.inc.php
@@ -1,11 +1,19 @@
 <?PHP
 include_once 'dbh.inc.php';
-if (isset($_POST['submit']))
+if (isset($_POST['LSubmit']))
 {
     $titelLabjournaal =  filter_input(INPUT_POST,'titelLabjournaal', FILTER_SANITIZE_SPECIAL_CHARS); 
+    $uitvoerders = filter_input(INPUT_POST,'uitvoerders', FILTER_SANITIZE_SPECIAL_CHARS);
     $experimentdatum = filter_input(INPUT_POST,'experimentdatum',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $experimentstartdatum = filter_input(INPUT_POST,'experimentstartdatum',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $experimenteinddatum = filter_input(INPUT_POST,'experimenteinddatum',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $uploadveiligheid = $_POST['uploadveiligheid'];
+    $doel = filter_input(INPUT_POST,'doel', FILTER_SANITIZE_SPECIAL_CHARS);
+    $uploadwaarnemingen = $_POST['uploadwaarnemingen'];
+    $hypothese = filter_input(INPUT_POST,'hypothese', FILTER_SANITIZE_SPECIAL_CHARS);
+    $materialen = filter_input(INPUT_POST,'materialen', FILTER_SANITIZE_SPECIAL_CHARS);
+    $methode = filter_input(INPUT_POST,'methode', FILTER_SANITIZE_SPECIAL_CHARS);
+    $uploadmeetresultaten = $_POST['uploadmeetresultaten'];
     $logboek = filter_input(INPUT_POST,'logboek',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $uploadlogboek = $_POST['uploadlogboek'];
     $observaties = filter_input(INPUT_POST,'observaties',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -13,22 +21,47 @@ if (isset($_POST['submit']))
     $weeggegevens = filter_input(INPUT_POST,'weeggegevens',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $uploadweeggegevens = $_POST['uploadweeggegevens'];
     $uploadafbeelding = $_POST['uploadafbeelding'];
-    $verplichteInput = array($titelLabjournaal,$experimentdatum);//studentnummer
+    $vakken = $_POST['LVak'];
+
+    // $afbeelding = file_get_contents($uploadwaarnemingen);
+    // $uploadwaarnemingendata = base64_encode($afbeelding);
+
+   
+    $verplichteInput = array
+    ($titelLabjournaal,$experimentdatum);//studentnummer
+    
     foreach($verplichteInput as $input)
     {
         if(empty($input))
         {
-            header("location: ../index.php?addLabjournaal=failed");
+            header("location: ../pages/labjournaalformulier.php?addLabjournaal=failed");
             DIE;
         }
     }
-    queryAanmaken('INSERT INTO labjournaal(studentID,docentID,labjournaalTitel,experimentDatum,experimentBeginDatum,
-    experimentEindDatum,logboek,bijlageLogboek,observaties,bijlageObservaties,weeggegevens,bijlageWeeggegevens,afbeelding)
+/*
+    queryAanmaken('
+    INSERT INTO labjournaal
+    (
+        studentID,docentID,labjournaalTitel,experimentDatum,experimentBeginDatum,
+        experimentEindDatum,logboek,observaties,weeggegevens,afbeeldingBron,bijlageLogboek,
+        bijlageObservaties,bijlageWeeggegevens,vakken,uitvoerder
+    
+    
+    ');*/
+ 
+
+
+
+ /*   queryAanmaken('INSERT INTO labjournaal(studentID,docentID,labjournaalTitel,experimentDatum,experimentBeginDatum,
+    experimentEindDatum,logboek,observaties,weeggegevens,/*afbeelding,figuur,*//*afbeeldingBron,bijlageLogboek,bijlageObservaties,
+    bijlageWeeggegevens,vakken,uitvoerders,veiligheid,hypothese,materialen,methode,meetresultaten,doel)
                    VALUES ("1","1","'.$titelLabjournaal.'","'.$experimentdatum.'","'.$experimentstartdatum.'"
-                   ,"'.$experimenteinddatum.'","'.$logboek.'","'.$uploadlogboek.'"
-                   ,"'.$observaties.'","'.$uploadobservaties.'","'.$weeggegevens.'"
-                   ,"'.$uploadweeggegevens.'","'.$uploadafbeelding.'");');
+                   ,"'.$experimenteinddatum.'","'.$logboek.'","'.$observaties.'"
+                   ,"'.$weeggegevens.'","'.$uploadafbeelding.'","'.$uploadlogboek.'"
+                   ,"'.$uploadobservaties.'","","'.$uploadweeggegevens.'","","'.$vakken.'","'.$uitvoerders.'","","
+                   ","'.$uploadveiligheid.'","","'.$hypothese.'","'.$materialen.'","
+                   ","'.$methode.'","","'.$uploadmeetresultaten.'","'.$doel.'");');
     querySluiten();
-	header("location: ../index.php?addLabjournaal=succes");    
+	header("location: ../pages/labjournaalformulier.php?addLabjournaal=succes");    */
 }
 
